6035236bb5e015a27c91160db094ac8de3508fb0
OohelloworldoO <sunny1234860@gmail.com>
2023-10-12 09:27:59 +0800

    CPE 1

--- CPE/CPE_100.c
+++ CPE/CPE_100.c
@@ -1,13 +1,18 @@
 #include <stdio.h>
 
-int aa(int a, int count)
+int al(int a, int count)
 {
     count++;
 
     if (a % 2 == 0)
-        return aa(a / 2, count);
+    {
+        return al(a / 2, count);
+    }
     else if (a % 2 == 1 && a > 1)
-        return aa(3 * a + 1, count);
+    {
+        return al(3 * a + 1, count);
+    }
+
     return count;
 }
 
@@ -16,4 +21,25 @@ int main()
     int i, j, a, b;
 
     while ((scanf("%d %d", &i, &j)) != EOF)
+    {
+        int max = 0;
+        a = i;
+        b = j;
+
+        if (j < i)
+        {
+            a = j;
+            b = i;
+        }
+
+        for (int k = a; k <= b; k++)
+        {
+
+            if (al(k, 0) > max)
+            {
+                max = al(k, 0);
+            }
+        }
+        printf("%d %d %d\n", i, j, max);
+    }
 }
