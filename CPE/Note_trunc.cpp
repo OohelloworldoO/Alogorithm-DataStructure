5c175bc46e8dc0ff5894aff060475f7ef4f5b3da
OohelloworldoO <sunny1234860@gmail.com>
2023-11-15 14:06:45 +0800

    update work

---
+++ CPE/Note_trunc.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include<cmath>
+using namespace std;
+int main()
+{
+    double x=3.14159,result_Show;
+    result_Show=trunc(x);
+    cout<<"trunc(x)="<<result_Show<<endl;
+
+    double xx=0.6678;
+    result_Show=trunc(xx);
+    cout<<"trunc(x)="<<result_Show<<endl;
+
+}
