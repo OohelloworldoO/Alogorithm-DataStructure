b34f0385a5c3c4553d87bd329f2f03082a09b39d
OohelloworldoO <sunny1234860@gmail.com>
2024-02-18 03:24:35 +0800

    update

---
+++ leetcode/cin_ignore_test.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    char str[30];
+    cout << "plz enter a string: ";
+    cin >> str;
+    cout << str << endl;
+    cin.ignore();
+    cout << "enter anything to end the program" << endl;
+    cin.get();
+}
