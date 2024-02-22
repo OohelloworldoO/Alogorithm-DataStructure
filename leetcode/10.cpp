be91c3bfdcc16dcc6d716cc9ca27668995f5706b
OohelloworldoO <sunny1234860@gmail.com>
2024-02-22 10:49:31 +0800

    update

--- leetcode/10.cpp
+++ leetcode/10.cpp
@@ -15,23 +15,24 @@ int main()
             cout << "Not jolly" << endl;
             break;
         }
+
         for (int i = 0; i < a; i++)
             cin >> b[i];
 
         for (int i = 0; i < a - 1; i++)
-        {
             c[i] = abs(b[i] - b[i + 1]);
-        }
-        for (int i = 0; i < a - 1; i++)
-        {
+
+        for (int i = 0; i < a - 1; i++) // 先跑一遍matrxi 0~n-1 如果有 = 1~n-1數字的 flag+1，break(不重複+1)
             for (int j = 0; j < a - 1; j++)
             {
                 if (c[j] == i + 1)
+                {
                     flag++;
+                    break;
+                }
             }
-        }
 
-        if (flag >= a - 1)
+        if (flag == a - 1)
             cout << "Jolly" << endl;
         else
             cout << "Not jolly" << endl;
