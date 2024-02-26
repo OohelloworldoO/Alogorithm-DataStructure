faf36e15be4cd9adc39ffd5c826ff6242c9e9802
OohelloworldoO <sunny1234860@gmail.com>
2024-02-26 23:33:22 +0800

    update

--- leetcode/Two Sum.cpp
+++ leetcode/Two Sum.cpp
@@ -1,3 +1,7 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// 方法1 : 雙迴圈 但效率低 因為是窮舉法(暴力法) 時間複雜度為 O(n²)
 class Solution
 {
 public:
@@ -16,3 +20,18 @@ public:
         return {};
     }
 };
+
+// 方法2:
+
+int main()
+{
+    vector<int> nums = {2, 7, 11, 15};
+    int target = 9; // [0,1]
+    // vector<int> nums = {3,2,4}; int target = 6; // [1,2]
+    // vector<int> nums = {3,3}; int target = 6; // [0,1]
+    Solution s;
+    vector<int> ret = s.twoSum(nums, target);
+    cout << "[" << ret[0] << "," << ret[1] << "]\n";
+
+    return 0;
+}
